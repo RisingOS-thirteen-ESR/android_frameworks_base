773a7026352b1b91665b2a025bec40c3bfeedd11
Praditia Nur <pnplusplus@gmail.com>
2024-03-02 23:44:15 +0000

    PIHooks: Reimplement games prop
    Change-Id: I587531de99a5915c1235c026f597faa6a1d1a161

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -34,6 +34,8 @@ import com.android.internal.R;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 
 public class PropImitationHooks {
 
@@ -54,6 +56,74 @@ public class PropImitationHooks {
 
     private static final ComponentName GMS_ADD_ACCOUNT_ACTIVITY = ComponentName.unflattenFromString(
             "com.google.android.gms/.auth.uiflows.minutemaid.MinuteMaidActivity");
+
+    private static final Map<String, Object> sP7Props = createGoogleSpoofProps("cheetah", "Pixel 7 Pro", sP7PFp);
+    private static final Map<String, Object> sPFoldProps = createGoogleSpoofProps("felix", "Pixel Fold", sFelixFp);
+    private static final Map<String, Object> sPTabletProps = createGoogleSpoofProps("tangorpro", "Pixel Tablet", "google/tangorpro/tangorpro:13/TQ3A.230901.001.B1/10750577:user/release-keys");
+    private static final Map<String, Object> gPhotosProps = createGoogleSpoofProps("marlin", "Pixel XL", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys");
+    private static final Map<String, Object> redfinProps = createGoogleSpoofProps("redfin", "Pixel 5", "google/redfin/redfin:13/TQ3A.230605.011/10161073:user/release-keys");
+    private static final Map<String, Object> asusROG1Props = createGameProps("ASUS_Z01QD", "Asus");
+    private static final Map<String, Object> asusROG3Props = createGameProps("ASUS_I003D", "Asus");
+    private static final Map<String, Object> xperia5Props = createGameProps("SO-52A", "Sony");
+    private static final Map<String, Object> op8ProProps = createGameProps("IN2020", "OnePlus");
+    private static final Map<String, Object> op9RProps = createGameProps("LE2101", "OnePlus");
+    private static final Map<String, Object> xmMi11TProps = createGameProps("21081111RG", "Xiaomi");
+    private static final Map<String, Object> xmF4Props = createGameProps("22021211RG", "Xiaomi");
+
+    private static Map<String, Object> createGameProps(String model, String manufacturer) {
+        Map<String, Object> props = new HashMap<>();
+        props.put("MODEL", model);
+        props.put("MANUFACTURER", manufacturer);
+        return props;
+    }
+
+    private static final Set<String> packagesToChangeROG1 = new HashSet<>(Arrays.asList(
+            "com.madfingergames.legends"
+    ));
+
+    private static final Set<String> packagesToChangeROG3 = new HashSet<>(Arrays.asList(
+            "com.pearlabyss.blackdesertm",
+            "com.pearlabyss.blackdesertm.gl"
+    ));
+
+    private static final Set<String> packagesToChangeXP5 = new HashSet<>(Arrays.asList(
+            "com.activision.callofduty.shooter",
+            "com.garena.game.codm",
+            "com.tencent.tmgp.kr.codm",
+            "com.vng.codmvn"
+    ));
+
+    private static final Set<String> packagesToChangeOP8P = new HashSet<>(Arrays.asList(
+            "com.netease.lztgglobal",
+            "com.pubg.imobile",
+            "com.pubg.krmobile",
+            "com.rekoo.pubgm",
+            "com.riotgames.league.wildrift",
+            "com.riotgames.league.wildrifttw",
+            "com.riotgames.league.wildriftvn",
+            "com.tencent.ig",
+            "com.tencent.tmgp.pubgmhd",
+            "com.vng.pubgmobile"
+    ));
+
+    private static final Set<String> packagesToChangeOP9R = new HashSet<>(Arrays.asList(
+            "com.epicgames.fortnite",
+            "com.epicgames.portal"
+    ));
+
+    private static final Set<String> packagesToChange11T = new HashSet<>(Arrays.asList(
+            "com.ea.gp.apexlegendsmobilefps",
+            "com.levelinfinite.hotta.gp",
+            "com.mobile.legends",
+            "com.supercell.clashofclans",
+            "com.tencent.tmgp.sgame",
+            "com.vng.mlbbvn"
+    ));
+
+    private static final Set<String> packagesToChangeF4 = new HashSet<>(Arrays.asList(
+            "com.dts.freefiremax",
+            "com.dts.freefireth"
+    ));
 
     private static volatile String sProcessName;
     private static volatile boolean sIsGms, sIsFinsky;
@@ -82,6 +152,35 @@ public class PropImitationHooks {
         } else if (!sStockFp.isEmpty() && packageName.equals(PACKAGE_ARCORE)) {
             dlog("Setting stock fingerprint for: " + packageName);
             setPropValue("FINGERPRINT", sStockFp);
+        } else {
+            if (SystemProperties.getBoolean("persist.sys.pixelprops.games", false)) {
+                Map<String, Object> gamePropsToSpoof = null;
+                if (packagesToChangeROG1.contains(packageName)) {
+                    dlog("Spoofing as Asus ROG 1 for: " + packageName);
+                    gamePropsToSpoof = asusROG1Props;
+                } else if (packagesToChangeROG3.contains(packageName)) {
+                    dlog("Spoofing as Asus ROG 3 for: " + packageName);
+                    gamePropsToSpoof = asusROG3Props;
+                } else if (packagesToChangeXP5.contains(packageName)) {
+                    dlog("Spoofing as Sony Xperia 5 for: " + packageName);
+                    gamePropsToSpoof = xperia5Props;
+                } else if (packagesToChangeOP8P.contains(packageName)) {
+                    dlog("Spoofing as Oneplus 8 Pro for: " + packageName);
+                    gamePropsToSpoof = op8ProProps;
+                } else if (packagesToChangeOP9R.contains(packageName)) {
+                    dlog("Spoofing as Oneplus 9R for: " + packageName);
+                    gamePropsToSpoof = op9RProps;
+                } else if (packagesToChange11T.contains(packageName)) {
+                    dlog("Spoofing as Xiaomi Mi 11T for: " + packageName);
+                    gamePropsToSpoof = xmMi11TProps;
+                } else if (packagesToChangeF4.contains(packageName)) {
+                    dlog("Spoofing as Xiaomi F4 for: " + packageName);
+                    gamePropsToSpoof = xmF4Props;
+                }
+                if (gamePropsToSpoof != null) {
+                    gamePropsToSpoof.forEach((k, v) -> setPropValue(k, v));
+                }
+            }
         }
     }
 
